2bb6a64317ebc4603f6f4715fe4c347d838fbd92
Ruben Boumans <ruben.boumans@paessler.com>
2023-12-04 14:25:18 +0100

    test: convert cube game examples into test cases

--- src/day_02.rs
+++ src/day_02.rs
@@ -50,3 +50,156 @@ pub fn run() {
     println!("Not implemented yet");
     unimplemented!();
 }
+
+#[derive(Debug, PartialEq)]
+struct Round {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+#[derive(Debug, PartialEq)]
+struct Game {
+    id: u32,
+    rounds: Vec<Round>,
+}
+
+fn convert_to_game(line: &str) -> Option<Game> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_convert_to_game_1() {
+        let line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+
+        let expected_game = Game {
+            id: 1,
+            rounds: vec![
+                Round {
+                    red: 4,
+                    green: 0,
+                    blue: 3,
+                },
+                Round {
+                    red: 1,
+                    green: 2,
+                    blue: 0,
+                },
+                Round {
+                    red: 0,
+                    green: 2,
+                    blue: 6,
+                },
+            ],
+        };
+
+        assert_eq!(convert_to_game(line), Some(expected_game));
+    }
+
+    #[test]
+    fn test_convert_to_game_2() {
+        let line = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
+        let expected_game = Game {
+            id: 2,
+            rounds: vec![
+                Round {
+                    red: 0,
+                    green: 2,
+                    blue: 1,
+                },
+                Round {
+                    red: 1,
+                    green: 3,
+                    blue: 4,
+                },
+                Round {
+                    red: 0,
+                    green: 1,
+                    blue: 1,
+                },
+            ],
+        };
+
+        assert_eq!(convert_to_game(line), Some(expected_game));
+    }
+
+    #[test]
+    fn test_convert_to_game_3() {
+        let line = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+        let expected_game = Game {
+            id: 3,
+            rounds: vec![
+                Round {
+                    red: 20,
+                    green: 8,
+                    blue: 6,
+                },
+                Round {
+                    red: 4,
+                    green: 13,
+                    blue: 5,
+                },
+                Round {
+                    red: 1,
+                    green: 5,
+                    blue: 0,
+                },
+            ],
+        };
+
+        assert_eq!(convert_to_game(line), Some(expected_game));
+    }
+
+    #[test]
+    fn test_convert_to_game_4() {
+        let line = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+        let expected_game = Game {
+            id: 4,
+            rounds: vec![
+                Round {
+                    red: 3,
+                    green: 1,
+                    blue: 6,
+                },
+                Round {
+                    red: 6,
+                    green: 3,
+                    blue: 0,
+                },
+                Round {
+                    red: 14,
+                    green: 3,
+                    blue: 15,
+                },
+            ],
+        };
+
+        assert_eq!(convert_to_game(line), Some(expected_game));
+    }
+
+    #[test]
+    fn test_convert_to_game_5() {
+        let line = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let expected_game = Game {
+            id: 5,
+            rounds: vec![
+                Round {
+                    red: 6,
+                    green: 3,
+                    blue: 1,
+                },
+                Round {
+                    red: 1,
+                    green: 2,
+                    blue: 2,
+                },
+            ],
+        };
+
+        assert_eq!(convert_to_game(line), Some(expected_game));
+    }
+}
