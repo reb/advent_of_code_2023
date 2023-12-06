ad8aa685f0a59cf0ca687dd2f1e2653509fa0e27
Ruben Boumans <ruben.boumans@paessler.com>
2023-12-06 14:18:16 +0100

    feat: parse the schematic into a usable format

--- src/day_03.rs
+++ src/day_03.rs
@@ -39,10 +39,84 @@
 ///
 /// Of course, the actual engine schematic is much larger. What is the sum of all of the part
 /// numbers in the engine schematic?
+use std::collections::HashMap;
 
 const INPUT: &str = include_str!("../input/day_03");
 
 pub fn run() {
     println!("Not implemented yet");
     unimplemented!();
+}
+
+type Point = (i32, i32);
+type Schematic = HashMap<Point, char>;
+
+fn parse_schematic(input: &str) -> Schematic {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(x, line)| {
+            line.char_indices()
+                .filter(|(_, c)| c != &'.')
+                .map(move |(y, c)| ((x as i32, y as i32), c))
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_schematic() {
+        let input = "\
+           467..114..\n\
+           ...*......\n\
+           ..35..633.\n\
+           ......#...\n\
+           617*......\n\
+           .....+.58.\n\
+           ..592.....\n\
+           ......755.\n\
+           ...$.*....\n\
+           .664.598..";
+
+        let mut expected_schematic = HashMap::new();
+        expected_schematic.insert((0, 0), '4');
+        expected_schematic.insert((0, 1), '6');
+        expected_schematic.insert((0, 2), '7');
+        expected_schematic.insert((0, 5), '1');
+        expected_schematic.insert((0, 6), '1');
+        expected_schematic.insert((0, 7), '4');
+        expected_schematic.insert((1, 3), '*');
+        expected_schematic.insert((2, 2), '3');
+        expected_schematic.insert((2, 3), '5');
+        expected_schematic.insert((2, 6), '6');
+        expected_schematic.insert((2, 7), '3');
+        expected_schematic.insert((2, 8), '3');
+        expected_schematic.insert((3, 6), '#');
+        expected_schematic.insert((4, 0), '6');
+        expected_schematic.insert((4, 1), '1');
+        expected_schematic.insert((4, 2), '7');
+        expected_schematic.insert((4, 3), '*');
+        expected_schematic.insert((5, 5), '+');
+        expected_schematic.insert((5, 7), '5');
+        expected_schematic.insert((5, 8), '8');
+        expected_schematic.insert((6, 2), '5');
+        expected_schematic.insert((6, 3), '9');
+        expected_schematic.insert((6, 4), '2');
+        expected_schematic.insert((7, 6), '7');
+        expected_schematic.insert((7, 7), '5');
+        expected_schematic.insert((7, 8), '5');
+        expected_schematic.insert((8, 3), '$');
+        expected_schematic.insert((8, 5), '*');
+        expected_schematic.insert((9, 1), '6');
+        expected_schematic.insert((9, 2), '6');
+        expected_schematic.insert((9, 3), '4');
+        expected_schematic.insert((9, 5), '5');
+        expected_schematic.insert((9, 6), '9');
+        expected_schematic.insert((9, 7), '8');
+
+        assert_eq!(parse_schematic(input), expected_schematic);
+    }
 }
