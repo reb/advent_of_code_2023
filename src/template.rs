807a0d4f3f073b4389a96bd013e64a48d041d840
Ruben Boumans <ruben.boumans@paessler.com>
2023-12-06 13:23:59 +0100

    docs: update template with an empty test case

--- src/template.rs
+++ src/template.rs
@@ -1,5 +1,5 @@
 /// --- Day xx: _ ---
-/// 
+///
 /// Problem text
 
 const INPUT: &str = include_str!("../input/day_xx");
@@ -7,4 +7,14 @@ const INPUT: &str = include_str!("../input/day_xx");
 pub fn run() {
     println!("Not implemented yet");
     unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_() {
+        assert_eq!("", "")
+    }
 }
