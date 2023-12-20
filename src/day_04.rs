d153a5be378f0e3e201d1d1d3aeca83756a9a093
Ruben Boumans <ruben.boumans@paessler.com>
2023-12-20 15:49:08 +0100

    test: Extract scratchcard parsing test cases

--- src/day_04.rs
+++ src/day_04.rs
@@ -54,6 +54,9 @@
 /// So, in this example, the Elf's pile of scratchcards is worth 13 points.
 ///
 /// Take a seat in the large pile of colorful cards. How many points are they worth in total?
+use nom::IResult;
+use std::collections::HashSet;
+
 const INPUT: &str = include_str!("../input/day_04");
 
 pub fn run() {
@@ -61,12 +64,91 @@ pub fn run() {
     unimplemented!();
 }
 
+#[derive(Debug, PartialEq)]
+struct Scratchcard {
+    winning_numbers: HashSet<u32>,
+    numbers: HashSet<u32>,
+}
+
+impl Scratchcard {
+    fn parse(input: &str) -> IResult<&str, Scratchcard> {
+        Ok((
+            "",
+            Scratchcard {
+                winning_numbers: HashSet::new(),
+                numbers: HashSet::new(),
+            },
+        ))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_() {
-        assert_eq!("", "")
+    fn test_scratchcard_parse_1() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+        let expected_scratchcard = Scratchcard {
+            winning_numbers: vec![41, 48, 83, 86, 17].into_iter().collect(),
+            numbers: vec![83, 86, 6, 31, 17, 9, 48, 53].into_iter().collect(),
+        };
+
+        assert_eq!(Scratchcard::parse(input), Ok(("", expected_scratchcard)));
+    }
+
+    #[test]
+    fn test_scratchcard_parse_2() {
+        let input = "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19";
+        let expected_scratchcard = Scratchcard {
+            winning_numbers: vec![13, 32, 20, 16, 61].into_iter().collect(),
+            numbers: vec![61, 30, 68, 82, 17, 32, 24, 19].into_iter().collect(),
+        };
+
+        assert_eq!(Scratchcard::parse(input), Ok(("", expected_scratchcard)));
+    }
+
+    #[test]
+    fn test_scratchcard_parse_3() {
+        let input = "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1";
+        let expected_scratchcard = Scratchcard {
+            winning_numbers: vec![1, 21, 53, 59, 44].into_iter().collect(),
+            numbers: vec![69, 82, 63, 72, 16, 21, 14, 1].into_iter().collect(),
+        };
+
+        assert_eq!(Scratchcard::parse(input), Ok(("", expected_scratchcard)));
+    }
+
+    #[test]
+    fn test_scratchcard_parse_4() {
+        let input = "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83";
+        let expected_scratchcard = Scratchcard {
+            winning_numbers: vec![41, 92, 73, 84, 69].into_iter().collect(),
+            numbers: vec![59, 84, 76, 51, 58, 5, 54, 83].into_iter().collect(),
+        };
+
+        assert_eq!(Scratchcard::parse(input), Ok(("", expected_scratchcard)));
+    }
+
+    #[test]
+    fn test_scratchcard_parse_5() {
+        let input = "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36";
+        let expected_scratchcard = Scratchcard {
+            winning_numbers: vec![87, 83, 26, 28, 32].into_iter().collect(),
+            numbers: vec![88, 30, 70, 12, 93, 22, 82, 36].into_iter().collect(),
+        };
+
+        assert_eq!(Scratchcard::parse(input), Ok(("", expected_scratchcard)));
+    }
+
+    #[test]
+    fn test_scratchcard_parse_6() {
+        let input = "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        let expected_scratchcard = Scratchcard {
+            winning_numbers: vec![31, 18, 13, 56, 72].into_iter().collect(),
+            numbers: vec![74, 77, 10, 23, 35, 67, 36, 11].into_iter().collect(),
+        };
+
+        assert_eq!(Scratchcard::parse(input), Ok(("", expected_scratchcard)));
     }
 }
