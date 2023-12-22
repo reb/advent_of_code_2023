255e1c8fb3257efc5637cdff3d936547f4cebaca
Ruben Boumans <ruben.boumans@paessler.com>
2023-12-22 12:05:05 +0100

    test: extract example of amount of scratchcards won

--- src/day_04.rs
+++ src/day_04.rs
@@ -162,6 +162,10 @@ impl Scratchcard {
     }
 }
 
+fn amount_of_scratchcards_won(scratchcards: &Vec<Scratchcard>) -> u32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -290,5 +294,37 @@ mod tests {
         };
 
         assert_eq!(scratchcard.score(), 0);
+    }
+
+    #[test]
+    fn test_amount_of_scratchcards_won() {
+        let scratchcards = vec![
+            Scratchcard {
+                winning_numbers: vec![41, 48, 83, 86, 17].into_iter().collect(),
+                numbers: vec![83, 86, 6, 31, 17, 9, 48, 53].into_iter().collect(),
+            },
+            Scratchcard {
+                winning_numbers: vec![13, 32, 20, 16, 61].into_iter().collect(),
+                numbers: vec![61, 30, 68, 82, 17, 32, 24, 19].into_iter().collect(),
+            },
+            Scratchcard {
+                winning_numbers: vec![1, 21, 53, 59, 44].into_iter().collect(),
+                numbers: vec![69, 82, 63, 72, 16, 21, 14, 1].into_iter().collect(),
+            },
+            Scratchcard {
+                winning_numbers: vec![41, 92, 73, 84, 69].into_iter().collect(),
+                numbers: vec![59, 84, 76, 51, 58, 5, 54, 83].into_iter().collect(),
+            },
+            Scratchcard {
+                winning_numbers: vec![87, 83, 26, 28, 32].into_iter().collect(),
+                numbers: vec![88, 30, 70, 12, 93, 22, 82, 36].into_iter().collect(),
+            },
+            Scratchcard {
+                winning_numbers: vec![31, 18, 13, 56, 72].into_iter().collect(),
+                numbers: vec![74, 77, 10, 23, 35, 67, 36, 11].into_iter().collect(),
+            },
+        ];
+
+        assert_eq!(amount_of_scratchcards_won(&scratchcards), 30);
     }
 }
