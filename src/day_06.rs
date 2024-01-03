e0c932a45174cc83d895c0b8c064a18cb991c35c
Ruben Boumans <ruben.boumans@paessler.com>
2024-01-03 13:38:19 +0100

    docs: there is only 1 big race

--- src/day_06.rs
+++ src/day_06.rs
@@ -72,6 +72,29 @@
 ///
 /// Determine the number of ways you could beat the record in each race. What do you get if you
 /// multiply these numbers together?
+///
+/// --- Part Two ---
+///
+/// As the race is about to start, you realize the piece of paper with race times and record
+/// distances you got earlier actually just has very bad kerning. There's really only one race -
+/// ignore the spaces between the numbers on each line.
+///
+/// So, the example from before:
+///
+/// Time:      7  15   30
+/// Distance:  9  40  200
+///
+/// ...now instead means this:
+///
+/// Time:      71530
+/// Distance:  940200
+///
+/// Now, you have to figure out how many ways there are to win this single race. In this example,
+/// the race lasts for 71530 milliseconds and the record distance you need to beat is 940200
+/// millimeters. You could hold the button anywhere from 14 to 71516 milliseconds and beat the
+/// record, a total of 71503 ways!
+///
+/// How many ways can you beat the record in this one much longer race?
 use std::ops::RangeInclusive;
 
 pub fn run() {
@@ -109,11 +132,19 @@ pub fn run() {
         "The number of ways you could win each race multiplied gives: {}",
         opportunities_multiplied
     );
+
+    // small input, still hardcode
+    // Time:        53     89     76     98
+    // Distance:   313   1090   1214   1201
+    let big_race = Race {
+        time: 53897698,
+        record_distance: 313109012141201,
+    };
 }
 
 struct Race {
-    time: u32,
-    record_distance: u32,
+    time: u64,
+    record_distance: u64,
 }
 
 impl Race {
